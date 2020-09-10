28dede0c652ebdaa80bfc0e3e5b07b04b13db3e1
zibuyu69 <37107112+zibuyu69@users.noreply.github.com>
2020-09-10 09:03:36 +0800

    li
    li

--- src/pages/newsList/index.js
+++ src/pages/newsList/index.js
@@ -3,20 +3,67 @@ import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import "./style.less";
 import * as actionCreators from "./store/actionCreators";
-import { Spin, Button } from "antd";
+import { Spin, Button, Icon, Tooltip, Card } from "antd";
+import { NavBar, Grid } from "antd-mobile";
 
 function NewsList(props) {
   const { loading, history } = props;
+  const [toShow, setToShow] = useState(false); // 是否展示更多菜单
+  const data = Array.from(new Array(9)).map((_val, i) => ({
+    icon: "https://gw.alipayobjects.com/zos/rmsportal/nywPmnTAvTmLusPxHPSu.png",
+    text: `name${i}`,
+  }));
 
-  const testClick = () => {
-    props.mergeData({
-      type: "registerReducer/MERGE_DATA",
-      payload: { test: props.state.test + 1 },
-    });
+  const gridStyle = {
+    width: "25%",
+    textAlign: "center",
   };
+
+  // 点击更多
+  const showMore = () => {
+    setToShow(!toShow);
+  };
+  // 所有的菜单
+  const allMenu = () => {
+    return (
+      <Card title="Card Title">
+        <Card.Grid style={gridStyle}>Content</Card.Grid>
+        <Card.Grid hoverable={false} style={gridStyle}>
+          Content
+        </Card.Grid>
+        <Card.Grid style={gridStyle}>Content</Card.Grid>
+        <Card.Grid style={gridStyle}>Content</Card.Grid>
+        <Card.Grid style={gridStyle}>Content</Card.Grid>
+        <Card.Grid style={gridStyle}>Content</Card.Grid>
+        <Card.Grid style={gridStyle}>Content</Card.Grid>
+      </Card>
+    );
+  };
+
   return (
     <div className="NewsList">
-      <div className="title">/newsList/newsList/newsList</div>
+      <div className="title">
+        <NavBar
+          mode="light"
+          icon={<Icon type="arrow-left" />}
+          onLeftClick={() => {
+            history.go(-1);
+          }}
+          rightContent={[<Icon key="sync" type="sync" /* spin */ />]}
+        >
+          <Tooltip
+            placement="bottom"
+            title={<Grid data={data} activeStyle={false} />}
+            trigger="click"
+            overlayClassName="NewsList_tooltip"
+          >
+            <div onClick={showMore}>
+              选择分类
+              <Icon type="caret-down" style={{ marginLeft: "8px" }} />
+            </div>
+          </Tooltip>
+        </NavBar>
+      </div>
     </div>
   );
 }
